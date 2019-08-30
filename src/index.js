0d61bff8c76e76df9e901cb1409cb18820363d1a
Marcin Maslanka <marcin.maslanka@brainhub.pl>
2019-08-30 07:52:37 +0200

    Add first server file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => null
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log('Server is running on port 4000'));
