cc062f9ecec54ec48d0918abbd7365e21db48bdf
Marcin Maslanka <marcin.maslanka@brainhub.pl>
2019-09-03 12:33:13 +0200

    Add prisma client and use context

--- src/index.js
+++ src/index.js
@@ -2,39 +2,27 @@ const { GraphQLServer } = require('graphql-yoga');
 
 const { prisma } = require('./generated/prisma-client');
 
-const links = [
-  {
-    id: 0,
-    description: 'First Link',
-    url: 'www.first-link.com'
-  }
-];
-
-let linksCount = links.length;
-
 const resolvers = {
   Query: {
     info: () => 'this is string',
-    feed: async () => await prisma.links()
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
   },
   Mutation: {
-    post: async (parent, args) => {
-      const link = {
-        description: args.description,
-        url: args.url
-      };
-
-      const newLink = await prisma.createLink(link);
-      console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
-
-      return newLink;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log('Server is running on port 4000'));
