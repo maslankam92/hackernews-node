c4ad8ef5578e6d9eeab579f534939b9d22f671b3
Marcin Maslanka <marcin.maslanka@brainhub.pl>
2019-09-03 11:01:32 +0200

    Add schema

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
+const links = [
+  {
+    id: 0,
+    description: 'First Link',
+    url: 'www.first-link.com'
   }
-`;
+];
 
 const resolvers = {
   Query: {
-    info: () => null
+    info: () => 'this is string',
+    feed: () => links
   }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers
+});
+
 server.start(() => console.log('Server is running on port 4000'));
