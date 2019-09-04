92085ec588014751535d1bbffc6684a255d7486b
Marcin Maslanka <marcin.maslanka@brainhub.pl>
2019-09-04 09:53:13 +0200

    Add filtering to feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,14 @@
-const feed = (parent, args, context) => {
-  return context.prisma.links();
+const feed = async (parent, args, context) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {};
+
+  return await context.prisma.links({ where });
 };
 
 module.exports = {
